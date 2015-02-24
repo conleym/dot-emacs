9fa45cc3167d96f1d9e1e3ec41e967c1a6b7fb3f
Mike Conley <mconley@courseload.com>
2015-02-24 12:02:13 -0500

    Always refresh package contents at restart to avoid 404s from repositories from rapidly-changing packages on startup.

--- lisp/conleym/conleym-packages.el
+++ lisp/conleym/conleym-packages.el
@@ -1,11 +1,14 @@
 (require 'conleym-elpa)
 (require 'conleym-el-get)
+(require 'package)
+
+;; Ensure package archive contents are current to avoid 404s from
+;; repositories due to rapidly-changing packages.
+(package-refresh-contents)
 
 ;; Ensure req-package and its dependencies are installed.
 (unless (require 'req-package "req-package" t)
   (progn
-    (require 'package)
-    (package-refresh-contents)
     (package-install 'req-package)))
 
 (require 'req-package)
@@ -13,6 +16,7 @@
 (require 'conleym-req-package)
 (require 'conleym-darwin-req-package)
 
+;; Load packages, installing any that are missing.
 (req-package-finish)
 
 (provide 'conleym-packages)
