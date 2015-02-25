e88d9abb6281de5eea578e0a134115c8ddd68106
Mike Conley <mconley@courseload.com>
2015-02-25 15:47:26 -0500

    Customize rainbow-mode's html color modes, mouse yank at point, and nextstep open frame behavior.

--- custom.el
+++ custom.el
@@ -23,6 +23,11 @@
  '(minibuffer-prompt-properties
    (quote
     (read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)) nil nil "Make minbuffer prompt read only (default) and don't allow the cursor into it (not default).")
+ '(mouse-yank-at-point t)
+ '(ns-pop-up-frames nil nil nil "Documents opened via Mac's open command show up in the current frame, not a new one.")
+ '(rainbow-html-colors-major-mode-list
+   (quote
+    (html-mode css-mode php-mode nxml-mode xml-mode web-mode)))
  '(require-final-newline t)
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
