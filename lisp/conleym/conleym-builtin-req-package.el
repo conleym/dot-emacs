4766b80dd443fa18ad56057064550998b38b9052
Mike Conley <mconley@courseload.com>
2015-03-15 18:13:42 -0400

    OK, setting those to tab-width was a bad idea.

--- lisp/conleym/conleym-builtin-req-package.el
+++ lisp/conleym/conleym-builtin-req-package.el
@@ -126,9 +126,7 @@ isn't supported in this major mode."
           (push '("<\\?xml" . nxml-mode) magic-mode-alist)
           (push '("<![dD][oO][cC][tT][yY][pP][eE]" . nxml-mode) magic-mode-alist))
   :config (progn
-            (setq nxml-attribute-indent tab-width
-                  nxml-slash-auto-complete-flag t
-                  nxml-child-indent tab-width)))
+            (setq nxml-slash-auto-complete-flag t)))
 
 (use-package rcirc
   :config (progn
