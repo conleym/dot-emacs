48b5288560a5f458ca06da2d649d2cdcd4322d44
Mike Conley <mconley@courseload.com>
2015-04-01 12:45:05 -0400

    Remove wordsmith-mode, since it seems not to work.

--- lisp/conleym/conleym-darwin-req-package.el
+++ lisp/conleym/conleym-darwin-req-package.el
@@ -5,9 +5,6 @@
    (progn
       ;; Must have Dash.app
       (req-package dash-at-point)
-      (req-package reveal-in-finder)
-      ;; Seems vaguely interesting. must have 'syn' installed, which I do.
-      (req-package wordsmith-mode
-        :defer t)))
+      (req-package reveal-in-finder)))
 
 (provide 'conleym-darwin-req-package)
