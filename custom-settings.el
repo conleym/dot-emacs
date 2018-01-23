f28a5c11c6e385055e72adc78d0d10a1aaddea9c
Mike Conley <conasdf@gmail.com>
2018-01-23 09:40:42 -0500

    Update custom settings -- no significant changes.

--- custom-settings.el
+++ custom-settings.el
@@ -8,17 +8,15 @@
  '(column-number-mode t)
  '(compilation-ask-about-save nil nil nil "Prompts are annoying. Of course I want to save the file before compiling it.")
  '(cua-mode t nil (cua-base))
- '(custom-enabled-themes (quote (wheatgrass)))
-  '(custom-safe-themes
-     (quote
-       ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+ '(custom-enabled-themes '(wheatgrass))
+ '(custom-safe-themes
+   '("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default))
  '(delete-by-moving-to-trash t)
  '(delete-old-versions t nil nil "We're keeping a lot of backups. No need to ask about getting rid of older ones.")
-  '(eimp-ignore-read-only-modes
-     (quote
-       (gnus-article-mode puzzle-mode tumme-display-image-mode tumme-thumbnail-mode w3m-mode xkcd-mode)))
- '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
- '(frame-background-mode (quote dark))
+ '(eimp-ignore-read-only-modes
+   '(gnus-article-mode puzzle-mode tumme-display-image-mode tumme-thumbnail-mode w3m-mode xkcd-mode))
+ '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)
+ '(frame-background-mode 'dark)
  '(gc-cons-threshold 20000000 nil nil "Suggestion from flx-ido author on improving performance: GC every 20MB instead of every .76MB.")
  '(global-linum-mode t)
  '(gnutls-min-prime-bits 1024)
@@ -28,24 +26,22 @@
  '(initial-scratch-message nil)
  '(large-file-warning-threshold 200000000 nil nil "Default (10 MB) is way too small. Let's try 200 MB instead.")
  '(midnight-mode t nil (midnight))
-  '(minibuffer-prompt-properties
-     (quote
-       (read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)) nil nil "Make minbuffer prompt read only (default) and don't allow the cursor into it (not default).")
+ '(minibuffer-prompt-properties
+   '(read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt) nil nil "Make minbuffer prompt read only (default) and don't allow the cursor into it (not default).")
  '(mouse-yank-at-point t)
  '(ns-pop-up-frames nil nil nil "Documents opened via Mac's open command show up in the current frame, not a new one.")
  '(package-enable-at-startup nil)
-  '(package-selected-packages
-     (quote
-       (ido-completing-read+ all-the-icons-dired all-the-icons crux adoc-mode auto-package-update oauth markdown-mode flycheck company cider zone-nyan yaml-mode yagist xkcd which-key web-mode vagrant-tramp vagrant unicode-troll-stopper undo-tree twittering-mode tumblesocks syslog-mode swift-mode sr-speedbar sourcemap smooth-scroll smex smartparens scss-mode sass-mode reveal-in-osx-finder req-package rainbow-mode pydoc puppet-mode pip-requirements pdf-tools paradox nyan-mode noflet minesweeper manage-minor-mode less-css-mode key-chord json-mode ido-ubiquitous hungry-delete graphviz-dot-mode gitignore-mode github-browse-file gitconfig-mode gitattributes-mode git-timemachine format-sql flycheck-pos-tip flycheck-clojure flx-ido floobits fill-column-indicator exec-path-from-shell ess es-mode emr emmet-mode elpy eimp editorconfig dired-imenu define-word dash-at-point company-tern company-auctex clj-refactor bug-hunter auto-compile auctex-latexmk async apples-mode anzu ansible-doc ag ace-jump-mode 2048-game)))
+ '(package-selected-packages
+   '(ido-completing-read+ all-the-icons-dired all-the-icons crux adoc-mode auto-package-update oauth markdown-mode flycheck company cider zone-nyan yaml-mode yagist xkcd which-key web-mode vagrant-tramp vagrant unicode-troll-stopper undo-tree twittering-mode tumblesocks syslog-mode swift-mode sr-speedbar sourcemap smooth-scroll smex smartparens scss-mode sass-mode reveal-in-osx-finder req-package rainbow-mode pydoc puppet-mode pip-requirements pdf-tools paradox nyan-mode noflet minesweeper manage-minor-mode less-css-mode key-chord json-mode ido-ubiquitous hungry-delete graphviz-dot-mode gitignore-mode github-browse-file gitconfig-mode gitattributes-mode git-timemachine format-sql flycheck-pos-tip flycheck-clojure flx-ido floobits fill-column-indicator exec-path-from-shell ess es-mode emr emmet-mode elpy eimp editorconfig dired-imenu define-word dash-at-point company-tern company-auctex clj-refactor bug-hunter auto-compile auctex-latexmk async apples-mode anzu ansible-doc ag ace-jump-mode 2048-game))
  '(require-final-newline t)
  '(save-interprogram-paste-before-kill t)
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
- '(show-paren-style (quote expression))
+ '(show-paren-style 'expression)
  '(size-indication-mode t)
  '(tab-width 4 nil nil "Who the hell uses a tab width of 8 (the default)?")
  '(tool-bar-mode nil)
- '(tramp-syntax (quote default) nil (tramp))
+ '(tramp-syntax 'default nil (tramp))
  '(use-dialog-box nil nil nil "The dialog box is annoying and, in some situations, works incorrectly on Mac."))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
