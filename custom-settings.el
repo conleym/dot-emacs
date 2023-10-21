f4f4871e64b7e3e4cb8171eb73acf14a92aa2319
Mike Conley <conleym@users.noreply.github.com>
2023-10-21 12:44:25 -0400

    Customize some of the new mouse settings.

--- custom-settings.el
+++ custom-settings.el
@@ -10,11 +10,13 @@
  '(cua-mode t nil (cua-base))
  '(custom-enabled-themes '(wheatgrass))
  '(custom-safe-themes
-   '("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default))
+   '("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26"
+     default))
  '(delete-by-moving-to-trash t)
  '(delete-old-versions t nil nil "We're keeping a lot of backups. No need to ask about getting rid of older ones.")
  '(eimp-ignore-read-only-modes
-   '(gnus-article-mode puzzle-mode tumme-display-image-mode tumme-thumbnail-mode w3m-mode xkcd-mode))
+   '(gnus-article-mode puzzle-mode tumme-display-image-mode
+                       tumme-thumbnail-mode w3m-mode xkcd-mode))
  '(enable-recursive-minibuffers t)
  '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)
  '(frame-background-mode 'dark)
@@ -29,12 +31,33 @@
  '(mac-pseudo-daemon-mode t)
  '(midnight-mode t nil (midnight))
  '(minibuffer-prompt-properties
-   '(read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt) nil nil "Make minbuffer prompt read only (default) and don't allow the cursor into it (not default).")
+   '(read-only t point-entered minibuffer-avoid-prompt face
+               minibuffer-prompt) nil nil "Make minbuffer prompt read only (default) and don't allow the cursor into it (not default).")
+ '(mouse-drag-and-drop-region-cross-program t nil nil "Allow dragging text to other programs.")
+ '(mouse-drag-copy-region 'non-empty nil nil "Only save mouse-selected text to kill ring if region is nonempty.")
+ '(mouse-drag-mode-line-buffer t nil nil "Allows dragging of the filename from the modeline to other programs.")
  '(mouse-yank-at-point t)
  '(ns-pop-up-frames nil nil nil "Documents opened via Mac's open command show up in the current frame, not a new one.")
  '(package-enable-at-startup nil)
  '(package-selected-packages
-   '(zone-nyan yaml-mode yagist xkcd which-key web-mode unicode-troll-stopper tide sr-speedbar smex smartparens reveal-in-osx-finder company-restclient restclient rainbow-delimiters rainbow-mode prettier-js pip-requirements pdf-tools paradox osx-location nyan-mode nov ns-auto-titlebar manage-minor-mode magit lsp-ui lsp-mode json-mode jq-mode imenu-list ido-completing-read+ hungry-delete git-modes git-timemachine flycheck-relint flycheck-pos-tip flycheck flx-ido fill-column-indicator es-mode emr emojify elpy eimp editorconfig dockerfile-mode docker dired-imenu define-word crux company-emoji company-auctex company browse-at-remote beacon auto-package-update auto-compile auctex-latexmk aggressive-indent all-the-icons-ibuffer all-the-icons-dired all-the-icons ag 2048-game exec-path-from-shell use-package-chords use-package-ensure-system-package delight use-package))
+   '(zone-nyan yaml-mode yagist xkcd which-key web-mode
+               unicode-troll-stopper tide sr-speedbar smex smartparens
+               reveal-in-osx-finder company-restclient restclient
+               rainbow-delimiters rainbow-mode prettier-js
+               pip-requirements pdf-tools paradox osx-location
+               nyan-mode nov ns-auto-titlebar manage-minor-mode magit
+               lsp-ui lsp-mode json-mode jq-mode imenu-list
+               ido-completing-read+ hungry-delete git-modes
+               git-timemachine flycheck-relint flycheck-pos-tip
+               flycheck flx-ido fill-column-indicator es-mode emr
+               emojify elpy eimp editorconfig dockerfile-mode docker
+               dired-imenu define-word crux company-emoji
+               company-auctex company browse-at-remote beacon
+               auto-package-update auto-compile auctex-latexmk
+               aggressive-indent all-the-icons-ibuffer
+               all-the-icons-dired all-the-icons ag 2048-game
+               exec-path-from-shell use-package-chords
+               use-package-ensure-system-package delight use-package))
  '(require-final-newline t)
  '(save-interprogram-paste-before-kill t)
  '(scroll-bar-mode nil)
