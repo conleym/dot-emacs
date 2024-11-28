a154d965d3bb89b871579c0a4f29407520d54e65
Mike Conley <conleym@users.noreply.github.com>
2024-11-28 03:10:16 -0500

    Don't try to install recently-builtin package.
    use-package-ensure-system-package is now built in to emacs, and has
    been removed from melpa.
    
    Install system-packages explicitly. We were already configuring it
    here, which wasn't really correct -- we should be configuring the
    package specified in use-package, not some package that it depends
    upon (as was the case previously).

--- lisp/conleym/conleym-packages.el
+++ lisp/conleym/conleym-packages.el
@@ -19,19 +19,28 @@
 
 ;; use-package configuration
 
+
 (setq use-package-verbose t)
 (setq use-package-always-ensure t)
 (setq use-package-compute-statistics t)
 
-;; implements the :delight keyword
-(use-package delight)
-
-;; implements the :ensure-system-package keyword. Mac only for now.
+;; system-packages is required by use-package-ensure-system-package.
+;; Configure the system package manager to use macports on mac. No config for
+;; other systems yet. Maybe someday.
 (if (conleym:is-darwin)
-    (use-package use-package-ensure-system-package
+    (use-package system-packages
+      ;; https://gitlab.com/jabranham/system-packages
       :config
       (setq system-packages-use-sudo t)
       (setq system-packages-package-manager 'port)))
+;; implements the :ensure-system-package keyword
+(use-package use-package-ensure-system-package
+  ;; do not try to install -- this is built-in, and, for unknown reasons, we
+  ;; get an error: Failed to install <package>: Package <package> is unavailable.
+  :ensure nil)
+
+;; implements the :delight keyword
+(use-package delight)
 
 ;; implements the :chord keyword
 (use-package use-package-chords
